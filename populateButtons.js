447468f2e23f0f65fbba7801f669d9663a073543
Dan Burnside <dburnside22@gmail.com>
2020-08-15 16:33:30 -0500

    added functions to populate the buttons on the page

--- populateButtons.js
+++ populateButtons.js
@@ -1,5 +1,29 @@
 const plantsAndSpaceing = []
+const buttonContainer = document.querySelector(".buttons-container");
 
-fetch("./squareFootGardenData.json")
-    .then(response => response.json())
-    .then(data => plantsAndSpaceing.push(...data["plantsAndSpaceing"]));
+function createButtonElement(plant) {
+    let button = document.createElement("button");
+    button.classList.add('plant-button')
+    button.innerHTML = plant.plantName;
+    button.addEventListener("click",function(){
+        updateSeedDisplay(plant.spacing);
+    })
+    return button;
+}
+
+
+function addButton(plant){
+    button = createButtonElement(plant);
+    buttonContainer.appendChild(button);
+}
+
+async function populateButtons() {
+    await fetch("./squareFootGardenData.json")
+            .then(response => response.json())
+            .then(data => plantsAndSpaceing.push(...data["plantsAndSpaceing"]))
+    plantsAndSpaceing.forEach(plant => {
+        addButton(plant)
+    });
+}    
+
+populateButtons();
