350f63416fc302342bb53e604211e2f44bd7a20a
Dan Burnside <dburnside22@gmail.com>
2020-08-15 14:15:07 -0500

    Added updateSeedDisplay function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let seedCount = 0;
-
+const seedContainer = document.querySelector('.square-container');
+const seedContainerClassNames = ['fourSeeds', 'nineSeeds', 'sixteenSeeds']
 
 function createSeedElement() {
     let seed = document.createElement("div");
@@ -8,7 +9,6 @@ function createSeedElement() {
 }
 
 function addSeed(){
-    const seedContainer = document.querySelector('.square-container');
     const seed = createSeedElement();
     seedContainer.appendChild(seed);
     seedCount++;
@@ -16,16 +16,31 @@ function addSeed(){
 
 function removeSeed(){
     if (seedCount > 0){
-        const seedContainer = document.querySelector('.square-container');
         seedContainer.removeChild(seedContainer.lastElementChild);
         seedCount--;
     }
 }
 
+function addAndRemoveClassNames(seedNumberToDisplay){
+    seedContainerClassNames.forEach(name => {
+        if(seedContainer.classList.contains(name)){
+            console.log(name);
+            seedContainer.classList.remove(name);
+        }
+    });
+    if (seedNumberToDisplay === 4) seedContainer.classList.add('fourSeeds'); 
+    if (seedNumberToDisplay === 9) seedContainer.classList.add('nineSeeds'); 
+    if (seedNumberToDisplay === 16) seedContainer.classList.add('sixteenSeeds'); 
+}
 
 
-function toggleSeedView(number){
-    console.log(number);
-    const seedCountContainer = document.querySelector(`div[data-seedCount="${number}"]`)
-    seedCountContainer.style.display = "grid";
+function updateSeedDisplay(seedNumberToDisplay){
+    if(seedNumberToDisplay >= 0){
+        if (seedNumberToDisplay != seedCount) {
+            addAndRemoveClassNames(seedNumberToDisplay)
+        }    
+        while(seedNumberToDisplay != (seedCount)){
+            seedNumberToDisplay - seedCount > 0 ? addSeed() : removeSeed();
+        }  
+    }
 }
