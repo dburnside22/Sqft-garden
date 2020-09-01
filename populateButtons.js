c28edcb6e6d44cb37316d5572abdf724cff76c33
Dan Burnside <dburnside22@gmail.com>
2020-08-31 19:35:00 -0500

    Absracted the button functionality a bit

--- populateButtons.js
+++ populateButtons.js
@@ -3,17 +3,32 @@ const buttonContainer = document.querySelector(".buttons-container");
 const searchBarInput = document.querySelector(".search-bar-input");
 let selectedButton;
 
+function buttonFunction(spacing, plantName, buttonClicked){
+    searchBarInput.innerHTML = "";
+    updateSeedDisplay(spacing);
+    updatePlantLabel(plantName);
+    toggleActive(buttonClicked)
+}
+
+function toggleActive(buttonClicked) {
+    if (selectedButton === buttonClicked) {
+        selectedButton.classList.remove('active');
+        selectedButton = "";
+        resetPlantLabel();
+        updateSeedDisplay(0);
+    } else {
+        if (selectedButton) selectedButton.classList.remove('active');
+        selectedButton = buttonClicked;
+        selectedButton.classList.add('active');
+    }
+}
 
 function createButtonElement(plant) {
     let button = document.createElement("button");
-    button.classList.add('plant-button')
+    button.classList.add('plant-button');
     button.innerHTML = plant.plantName;
     button.addEventListener("click", function(){
-        updateSeedDisplay(plant.spacing);
-        updatePlantLabel(plant.plantName);
-        if (selectedButton) selectedButton.classList.remove('active');
-        selectedButton = this;
-        this.classList.add('active');
+        buttonFunction(plant.spacing, plant.plantName, this);
     })
     return button;
 }
@@ -60,18 +75,21 @@ function resetButtons() {
     populateButtons(plantsAndSpaceing);
 }
 
-
 function filterButtons() {
     const searchingString = String(this.value).toLowerCase();
+    if (searchingString === ""){
+        return; 
+    }
     const newPlantList = plantsAndSpaceing.filter(record => {
         if (record.plantName.includes(searchingString)){
-            return record
+            return record;
         }
     });
+    resetPlantLabel();
     updateSeedDisplay(0);
     populateButtons(newPlantList);
 }
 
-searchBarInput.addEventListener("keyup", filterButtons)
+searchBarInput.addEventListener("keyup", filterButtons);
 
 getInitialButtons();
