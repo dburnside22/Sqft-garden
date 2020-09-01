8df404441872f20cfe760689454704232b65fa55
Dan Burnside <dburnside22@gmail.com>
2020-08-31 21:01:34 -0500

    Added a menu?

--- menu.js
+++ menu.js
@@ -1,13 +1,17 @@
 let isMenuOpen = false;
 const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+
 
 function toggleMenu(){
     if (isMenuOpen){
        // closeMenu();
         menuButton.classList.remove("is-active");
+        menu.classList.remove("is-active");
     } else {
         //openMenu();
         menuButton.classList.add("is-active");
+        menu.classList.add("is-active");
     }
     isMenuOpen = !isMenuOpen;
 }
