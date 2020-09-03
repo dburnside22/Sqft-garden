be5ac64291f4dedd14396ab3539323705aa8c99f
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-09-03 13:18:07 -0500

    move filter functions

--- populateButtons.js
+++ populateButtons.js
@@ -53,29 +53,7 @@ function populateButtons(buttons){
     });
 }
 
-function sortButtonsBySeedCount(seedCount){
-    updateSeedDisplay(seedCount);
-    const newPlantList = plantsAndSpaceing.filter(record => {
-        return record.spacing === seedCount
-    })
-    populateButtons(newPlantList);
-}
-
-function sortByMonth(month){
-    updateSeedDisplay(0);
-    const newPlantList = plantsAndSpaceing.filter(record => {
-        if (record.seedMonths.includes(month) || record.transplantMonths.includes(month)){
-            return record;
-        }
-    });
-    toggleMenu();
-    populateButtons(newPlantList);
-    clearSearchBar();
-    resetPlantLabel();
-    addMonthLabel(month);
-}
-
-function resetButtons() {
+function toggleResetActive() {
     if (refreshButton.classList.contains("is-active")){
         refreshButton.classList.remove("is-active");
     } else {
