0eb51cb0245d59ed890fba10c1d8a6bb52986a8c
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-11-06 16:15:17 -0600

    WIP adding the things to firebase

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,34 +1,16 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBmZ6bPsZIJCDxmWctWkcrvpAAMiC7FwDY",
-  authDomain: "plantdata-53c8c.firebaseapp.com",
-  databaseURL: "https://plantdata-53c8c.firebaseio.com",
-  projectId: "plantdata-53c8c",
-  storageBucket: "plantdata-53c8c.appspot.com",
-  messagingSenderId: "155688130868",
-  appId: "1:155688130868:web:522d224f27c254c5e1ad61",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.firestore();
+import { addPlant } from "./services/firebaseService.js";
+let allThePlants = [];
 
-function testing() {
-  database
-    .collection("plants")
-    .doc("kohlrabi")
-    .set({
-      plantName: "kohlrabi",
-      spacing: 4,
-      seedMonths: ["february", "march", "april", "august", "september"],
-      transplantMonths: ["february", "march", "april", "august", "september"],
-      houseMonths: ["december", "january"],
-    })
-    .then(() => {
-      console.log("it sent the data!");
-    })
-    .catch(() => {
-      console.log("it didn't work out");
-    });
+async function test() {
+	await fetch("refereneces/squareFootGardenData.json")
+		.then((response) => response.json())
+		.then((data) => allThePlants.push(...data["plantsAndSpaceing"]));
+	console.log(allThePlants);
+	allThePlants.forEach((plant) => {
+		addPlant(plant);
+	});
+	// addPlant(allThePlants[0]);
 }
 
+test();
 // testing();
