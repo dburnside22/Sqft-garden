9519b129621e2a39d3b325686058b0fd643b40b8
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-11-06 09:42:44 -0600

    Added basic fetch to get plantData

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,4 +1,19 @@
-console.log("here");
+let plantData = [];
+
+const monthButtonLabels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 // add a row with plant name, and lines for all three categories, with boxes for each month
 // each box should toggle the month and update the database
@@ -8,3 +23,12 @@ console.log("here");
 // Populate rows
 // get plantingMonthsByCategory
 //
+async function getInitialPlantData() {
+  await fetch("refereneces/squareFootGardenData.json")
+    .then((response) => response.json())
+    .then((data) => plantData.push(...data["plantsAndSpaceing"]));
+
+  console.log("plantData", plantData);
+}
+
+getInitialPlantData();
