f161de5aae09fbd6c3dd6399f49624c9497417be
Dan Burnside <dburnside22@gmail.com>
2020-11-10 19:02:03 -0600

    cominented out this function becasuse it loaded all the data I need

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,16 +1,16 @@
 import { addPlant } from "./services/firebaseService.js";
 let allThePlants = [];
 
-async function test() {
-	await fetch("refereneces/squareFootGardenData.json")
-		.then((response) => response.json())
-		.then((data) => allThePlants.push(...data["plantsAndSpaceing"]));
-	allThePlants.forEach((plant) => {
-    addPlant(plant);
-    console.log(`${plant.plantName} added to firebase`);
-	});
-	// addPlant(allThePlants[0]);
-}
+// async function test() {
+// 	await fetch("refereneces/squareFootGardenData.json")
+// 		.then((response) => response.json())
+// 		.then((data) => allThePlants.push(...data["plantsAndSpaceing"]));
+// 	allThePlants.forEach((plant) => {
+//     addPlant(plant);
+//     console.log(`${plant.plantName} added to firebase`);
+// 	});
+// 	// addPlant(allThePlants[0]);
+// }
 
-test();
+// test();
 // testing();
