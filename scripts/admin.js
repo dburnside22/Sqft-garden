ba90729355314f7fc03e280253ab515f143a9e49
Dan Burnside <dburnside22@gmail.com>
2020-11-10 19:36:36 -0600

    quailty of live refactors to make my life easier i think

--- scripts/admin.js
+++ scripts/admin.js
@@ -3,21 +3,26 @@ import { getAllPlantData } from "./services/firebaseService.js";
 let plantData = [];
 const adminView = document.querySelector(".admin-view");
 
-const icons = ["house", "plant", "seed"];
+const plantingMonthData = [
+	{"icon": "house", "plantingType": "houseMonths"},
+	{"icon": "plant", "plantingType": "transplantMonths"},
+	{"icon": "seed", "plantingType": "seedMonths"},
+
+];
 
 const monthButtonLabels = [
-	"Jan",
-	"Feb",
-	"Mar",
-	"Apr",
-	"May",
-	"Jun",
-	"Jul",
-	"Aug",
-	"Sep",
-	"Oct",
-	"Nov",
-	"Dec",
+	"janunary",
+	"feburary",
+	"march",
+	"april",
+	"may",
+	"june",
+	"july",
+	"august",
+	"september",
+	"october",
+	"november",
+	"december",
 ];
 
 function toggleMonth(plantData, month, icon) {
@@ -25,19 +30,17 @@ function toggleMonth(plantData, month, icon) {
 	console.log(plantData, month, icon);
 }
 
-function createAdminRecord(plantInfo) {
-	let adminRecordContainer = document.createElement("div");
-	let recordTitle = document.createElement("h2");
-	recordTitle.innerHTML = plantInfo.plantName;
-	adminRecordContainer.appendChild(recordTitle);
+function createIcon(icon) {
+	let iconRow = document.createElement("div");
+	iconRow.classList.add("admin-view-row");
+	let iconImage = document.createElement("img");
+	iconImage.src = `./pngs/${icon}.png`;
+	iconRow.appendChild(iconImage);
+	return iconRow;
+}
 
-	icons.forEach((icon) => {
-		let iconRow = document.createElement("div");
-		iconRow.classList.add("admin-view-row");
-		let iconImage = document.createElement("img");
-		iconImage.src = `./pngs/${icon}.png`;
-		iconRow.appendChild(iconImage);
-		let calendarButtons = document.createElement("div");
+function createMonthButtonLabels(plantInfo, icon){
+	let calendarButtons = document.createElement("div");
 		monthButtonLabels.forEach((month) => {
 			let monthButton = document.createElement("button");
 			monthButton.innerHTML = `${month}`;
@@ -46,6 +49,18 @@ function createAdminRecord(plantInfo) {
 			});
 			calendarButtons.appendChild(monthButton);
 		});
+	return calendarButtons;
+}
+
+function createAdminRecord(plantInfo) {
+	let adminRecordContainer = document.createElement("div");
+	let recordTitle = document.createElement("h2");
+	recordTitle.innerHTML = plantInfo.plantName;
+	adminRecordContainer.appendChild(recordTitle);
+
+	plantingMonthData.forEach((data) => {
+		let iconRow = createIcon(data.icon);
+		let calendarButtons = createMonthButtonLabels(plantInfo, data.icon);
 		iconRow.appendChild(calendarButtons);
 		adminRecordContainer.appendChild(iconRow);
 	});
