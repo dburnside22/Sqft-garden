8c62a2685b56a40852a7304c806f75c9049fb206
Dan Burnside <dburnside22@gmail.com>
2020-11-15 14:13:19 -0600

    deleted this stuff becasue i didnt want a web of exports and imports

--- scripts/searchBar.js
+++
@@ -1,7 +0,0 @@
-const searchBarInput = document.querySelector(".search-bar-input");
-
-function clearSearchBar() {
-    searchBarInput.value = "";
-  }
-  
-  searchBarInput.addEventListener("keyup", filterButtons);
